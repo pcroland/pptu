ab173b0b037aad8e7ce3233cc564b4928c84b2b2
nyuszika7h <nyuszika7h@gmail.com>
2022-08-22 15:08:55 +0200

    BTN: Perform some replacements on release name for Atmos and HDR formats

--- pymkt/uploaders/broadcasthenet.py
+++ pymkt/uploaders/broadcasthenet.py
@@ -134,6 +134,16 @@ class BroadcasTheNetUploader(Uploader):
             sys.exit(1)
 
         release_name = path.stem if path.is_file() else path.name
+        release_name = re.sub(r"\.([a-z]+)\.?([\d.]+)\.Atmos", r"\.\1A\2", release_name, flags=re.I)
+        if m := re.search(r"\.(?:DV|DoVi)(?:\.HDR(?:10(?:\+|P|Plus))\b", release_name, flags=re.I):
+            release_name = release_name.replace(m.group(), "")
+            release_name = re.sub(r"\.(\d+p)", r".DV.\1", release_name)
+        elif m := re.search(r"\.(?:\.HDR(?:10(?:\+|P|Plus)))\b", release_name, flags=re.I):
+            release_name = release_name.replace(m.group(), "")
+            release_name = re.sub(r"\.(\d+p)", r".HDR.\1", release_name)
+        elif m := re.search(r"\.HLG\b", release_name, flags=re.I):
+            release_name = release_name.replace(m.group(), "")
+            release_name = re.sub(r"\.(\d+p)", r".HLG.\1", release_name)
 
         r = self.session.post(
             url="https://broadcasthe.net/upload.php",
