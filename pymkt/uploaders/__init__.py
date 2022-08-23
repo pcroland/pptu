815e7d6e94720556af6936865fb40658b39d0d90
nyuszika7h <nyuszika7h@gmail.com>
2022-08-24 00:57:01 +0200

    Enable CinemaZ and PrivateHD uploaders

--- pymkt/uploaders/__init__.py
+++ pymkt/uploaders/__init__.py
@@ -1,5 +1,7 @@
 from pymkt.uploaders._base import Uploader
 from pymkt.uploaders.avistaz import AvistaZUploader
 from pymkt.uploaders.broadcasthenet import BroadcasTheNetUploader
+from pymkt.uploaders.cinemaz import CinemaZUploader
 from pymkt.uploaders.hdbits import HDBitsUploader
 from pymkt.uploaders.passthepopcorn import PassThePopcornUploader
+from pymkt.uploaders.privatehd import PrivateHDUploader
