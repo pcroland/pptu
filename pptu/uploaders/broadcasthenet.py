15fdcf1d1abf4808839dab5b1953c2f463de60a2
nyuszika7h <nyuszika7h@gmail.com>
2022-08-29 01:45:05 +0200

    BTN: Fix imports

--- pptu/uploaders/broadcasthenet.py
+++ pptu/uploaders/broadcasthenet.py
@@ -17,8 +17,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from ..utils import eprint, load_html, wprint
 from . import Uploader
-from .utils import eprint, load_html, wprint
 
 
 class BroadcasTheNetUploader(Uploader):
