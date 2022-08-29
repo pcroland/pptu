b1f0b943031c850c51a76fa47a4eac86d14b9225
nyuszika7h <nyuszika7h@gmail.com>
2022-08-29 13:36:38 +0200

    cli: Split steps into functions

--- pptu/cli.py
+++ pptu/cli.py
@@ -26,6 +26,9 @@ from .constants import PROG_NAME, PROG_VERSION
 from .utils import Config, RParse, eprint
 
 
+dirs = PlatformDirs(appname="pptu", appauthor=False)
+
+
 def main():
     parser = RParse(prog=PROG_NAME)
     parser.add_argument("file",
@@ -46,8 +49,6 @@ def main():
                         help="upload without confirmation")
     args = parser.parse_args()
 
-    dirs = PlatformDirs(appname="pptu", appauthor=False)
-
     config = Config(dirs.user_config_path / "config.toml")
 
     session = requests.Session()
@@ -66,8 +67,6 @@ def main():
         )
 
     trackers = {}
-
-    trackers_json = importlib.resources.path("pptu", "trackers.json")
 
     print(r"[bold green]\[1/6] Logging in to trackers[/bold green]")
     for i, tracker_name in enumerate(copy(args.trackers)):
@@ -110,63 +109,21 @@ def main():
             eprint(f"File [cyan]{file.name!r}[/cyan] does not exist.")
             continue
 
-        d = dirs.user_cache_path / f"{file.name}_files"
-        d.mkdir(parents=True, exist_ok=True)
+        cache_dir = dirs.user_cache_path / f"{file.name}_files"
+
+        tracker_list = [trackers[t] for t in args.trackers]
 
         print("\n[bold green]\\[2/6] Creating torrent files[/bold green]")
-        base_torrent_path = d / f"{file.name}.torrent"
-        if not base_torrent_path.exists():
-            subprocess.run([
-                "torrenttools",
-                "create",
-                "--no-created-by",
-                "--no-creation-date",
-                "--no-cross-seed",
-                "--exclude", r".*\.(ffindex|jpg|nfo|png|srt|torrent|txt)$",
-                "-o",
-                base_torrent_path,
-                file,
-            ], check=True)
-
-        for tracker_name in args.trackers:
-            tracker = trackers[tracker_name]
-
-            with tempfile.NamedTemporaryFile(suffix=".yml") as tmp:
-                YAML().dump({
-                    "tracker-parameters": {
-                        tracker.name: {
-                            "pid": tracker().passkey,
-                        },
-                    },
-                }, tmp)
-                subprocess.run([
-                    "torrenttools",
-                    "--trackers-config", trackers_json,
-                    "--config", tmp.name,
-                    "edit",
-                    "--no-created-by",
-                    "--no-creation-date",
-                    "-a", tracker.name,
-                    "-s", next(
-                        x for x in json.loads(trackers_json.read_text()) if x["name"] == tracker.name
-                    )["source"],
-                    "-o", d / f"{file.name}[{tracker.abbrev}].torrent",
-                    d / f"{file.name}.torrent",
-                ], check=True)
+        create_torrent(file, tracker_list, cache_dir=cache_dir)
 
         cur_uploaders = []
         for i, tracker_name in enumerate(args.trackers):
             tracker = trackers[tracker_name]
             cur_uploaders.append(tracker)
+        has_all_files = any(x.all_files for x in cur_uploaders)
 
         print("\n[bold green]\\[3/6] Generating MediaInfo[/bold green]")
-        if file.is_file() or any(x.all_files for x in cur_uploaders):
-            f = file
-        else:
-            f = list(sorted([*file.glob("*.mkv"), *file.glob("*.mp4")]))[0]
-        p = subprocess.run(["mediainfo", f], cwd=file.parent, check=True, capture_output=True, encoding="utf-8")
-        mediainfo = [x.strip() for x in re.split(r"\n\n(?=General)", p.stdout)]
-
+        mediainfo = get_mediainfo(file, all_files=has_all_files)
         print("Done!")
 
         # Generating snapshots
@@ -174,80 +131,164 @@ def main():
             config.get("default", "snapshot_columns", 2) * config.get("default", "snapshot_rows", 2),
             max(x.min_snapshots for x in cur_uploaders),
         )
-        has_all_files = any(x.all_files for x in cur_uploaders)
-        if file.is_dir() or has_all_files:
-            # TODO: Handle case when number of files < num_snapshots
-            files = list(sorted([*file.glob("*.mkv"), *file.glob("*.mp4")]))
-        elif file.is_file():
-            files = [file] * num_snapshots
-        if has_all_files:
-            num_snapshots = len(files)
-        snapshots = []
-        print()
-        for i in track(range(num_snapshots), description="[bold green]\\[4/6] Generating snapshots[/bold green]"):
-            mediainfo_obj = MediaInfo.parse(files[i])
-            duration = float(mediainfo_obj.video_tracks[0].duration) / 1000
-            interval = duration / (num_snapshots + 1)
-
-            snap = d / f"{(i + 1):02}.png"
-            if not snap.exists():
-                subprocess.run([
-                    "ffmpeg",
-                    "-y",
-                    "-v", "error",
-                    "-ss", str(interval * (i + 1) if len(set(files)) == 1 else duration / 2),
-                    "-i", files[i],
-                    "-vf", "scale='max(sar,1)*iw':'max(1/sar,1)*ih'",
-                    "-frames:v", "1",
-                    snap,
-                ], check=True)
-                with Image(filename=snap) as img:
-                    img.depth = 8
-                    img.save(filename=snap)
-                oxipng.optimize(snap)
-            snapshots.append(snap)
+        snapshots = generate_snapshots(file, num_snapshots, cache_dir=cache_dir, all_files=has_all_files)
 
         print("\n[bold green]\\[5/6] Generating thumbnails[/bold green]")
-        thumbnails = []
-        for i in range(num_snapshots):
-            thumb = d / f"{(i + 1):02}_thumb.png"
-            if not thumb.exists():
-                with Image(filename=snapshots[i]) as img:
-                    img.resize(300, round(img.height / (img.width / 300)))
-                    img.depth = 8
-                    img.save(filename=thumb)
-                oxipng.optimize(thumb)
-            thumbnails.append(thumb)
+        thumbnails = generate_thumbnails(file, snapshots, cache_dir=cache_dir)
         print("Done!")
 
         print("\n[bold green]\\[6/6] Uploading[/bold green]")
-        for i, tracker_name in enumerate(args.trackers):
-            tracker = trackers[tracker_name]
-            print(f"[bold cyan]\\[{i + 1}/{len(args.trackers)}] Uploading to {tracker.abbrev}[/bold cyan]")
-            uploader = tracker()
-            mediainfo_tmp = mediainfo
-            snapshots_tmp = snapshots
-            if not tracker.all_files:
-                mediainfo_tmp = mediainfo[0]
-                snapshots_tmp = snapshots[:num_snapshots]
-            if uploader.upload(file, mediainfo_tmp, snapshots_tmp, thumbnails, auto=args.auto):
-                torrent_path = d / f"{file.name}[{tracker.abbrev}].torrent"
-                if watch_dir := config.get(tracker, "watch_dir"):
-                    watch_dir = Path(watch_dir).expanduser()
-                    subprocess.run(
-                        [
-                            sys.executable,
-                            importlib.resources.path("pyrosimple.scripts", "chtor.py"),
-                            "-H", file,
-                            torrent_path,
-                        ],
-                        env={**os.environ, "PYRO_RTORRENT_RC": os.devnull},
-                        check=True,
-                    )
-                    shutil.copyfile(torrent_path, watch_dir / torrent_path.name)
-            else:
-                eprint(f"Upload to [cyan]{tracker.name}[/cyan] failed.")
-            print()
+        upload(
+            file,
+            tracker_list,
+            mediainfo,
+            snapshots,
+            thumbnails,
+            num_snapshots,
+            config=config,
+            cache_dir=cache_dir,
+            auto=args.auto,
+        )
+
+        print()
+
+
+def create_torrent(file, trackers, *, cache_dir):
+    base_torrent_path = cache_dir / f"{file.name}.torrent"
+    if not base_torrent_path.exists():
+        subprocess.run([
+            "torrenttools",
+            "create",
+            "--no-created-by",
+            "--no-creation-date",
+            "--no-cross-seed",
+            "--exclude", r".*\.(ffindex|jpg|nfo|png|srt|torrent|txt)$",
+            "-o",
+            base_torrent_path,
+            file,
+        ], check=True)
+
+    trackers_json = importlib.resources.path("pptu", "trackers.json")
+    for tracker in trackers:
+        with tempfile.NamedTemporaryFile(suffix=".yml") as tmp:
+            YAML().dump({
+                "tracker-parameters": {
+                    tracker.name: {
+                        "pid": tracker().passkey,
+                    },
+                },
+            }, tmp)
+            subprocess.run([
+                "torrenttools",
+                "--trackers-config", trackers_json,
+                "--config", tmp.name,
+                "edit",
+                "--no-created-by",
+                "--no-creation-date",
+                "-a", tracker.name,
+                "-s", next(
+                    x for x in json.loads(trackers_json.read_text()) if x["name"] == tracker.name
+                )["source"],
+                "-o", cache_dir / f"{file.name}[{tracker.abbrev}].torrent",
+                cache_dir / f"{file.name}.torrent",
+            ], check=True)
+
+
+def get_mediainfo(file, *, all_files=False):
+    if file.is_file() or all_files:
+        f = file
+    else:
+        f = list(sorted([*file.glob("*.mkv"), *file.glob("*.mp4")]))[0]
+
+    p = subprocess.run(["mediainfo", f], cwd=file.parent, check=True, capture_output=True, encoding="utf-8")
+
+    mediainfo = [x.strip() for x in re.split(r"\n\n(?=General)", p.stdout)]
+    if not all_files:
+        mediainfo = mediainfo[0]
+    return mediainfo
+
+
+def generate_snapshots(file, num_snapshots, *, cache_dir, all_files=False):
+    if file.is_dir() or all_files:
+        # TODO: Handle case when number of files < num_snapshots
+        files = list(sorted([*file.glob("*.mkv"), *file.glob("*.mp4")]))
+    elif file.is_file():
+        files = [file] * num_snapshots
+    if all_files:
+        num_snapshots = len(files)
+
+    snapshots = []
+
+    print()
+    for i in track(range(num_snapshots), description="[bold green]\\[4/6] Generating snapshots[/bold green]"):
+        mediainfo_obj = MediaInfo.parse(files[i])
+        duration = float(mediainfo_obj.video_tracks[0].duration) / 1000
+        interval = duration / (num_snapshots + 1)
+
+        snap = cache_dir / f"{(i + 1):02}.png"
+        if not snap.exists():
+            subprocess.run([
+                "ffmpeg",
+                "-y",
+                "-v", "error",
+                "-ss", str(interval * (i + 1) if len(set(files)) == 1 else duration / 2),
+                "-i", files[i],
+                "-vf", "scale='max(sar,1)*iw':'max(1/sar,1)*ih'",
+                "-frames:v", "1",
+                snap,
+            ], check=True)
+            with Image(filename=snap) as img:
+                img.depth = 8
+                img.save(filename=snap)
+            oxipng.optimize(snap)
+        snapshots.append(snap)
+
+    return snapshots
+
+
+def generate_thumbnails(file, snapshots, *, cache_dir):
+    thumbnails = []
+    for i in range(len(snapshots)):
+        thumb = cache_dir / f"{(i + 1):02}_thumb.png"
+        if not thumb.exists():
+            with Image(filename=snapshots[i]) as img:
+                img.resize(300, round(img.height / (img.width / 300)))
+                img.depth = 8
+                img.save(filename=thumb)
+            oxipng.optimize(thumb)
+        thumbnails.append(thumb)
+    return thumbnails
+
+
+def upload(file, trackers, mediainfo, snapshots, thumbnails, num_snapshots, *, config, cache_dir, auto):
+    for i, tracker in enumerate(trackers):
+        print(f"[bold cyan]\\[{i + 1}/{len(trackers)}] Uploading to {tracker.abbrev}[/bold cyan]")
+        uploader = tracker()
+
+        mediainfo_tmp = mediainfo
+        snapshots_tmp = snapshots
+        if not tracker.all_files:
+            mediainfo_tmp = mediainfo[0]
+            snapshots_tmp = snapshots[:num_snapshots]
+
+        if not uploader.upload(file, mediainfo_tmp, snapshots_tmp, thumbnails, auto=auto):
+            eprint(f"Upload to [cyan]{tracker.name}[/cyan] failed.")
+            return
+
+        torrent_path = cache_dir / f"{file.name}[{tracker.abbrev}].torrent"
+        if watch_dir := config.get(tracker, "watch_dir"):
+            watch_dir = Path(watch_dir).expanduser()
+            subprocess.run(
+                [
+                    sys.executable,
+                    importlib.resources.path("pyrosimple.scripts", "chtor.py"),
+                    "-H", file,
+                    torrent_path,
+                ],
+                env={**os.environ, "PYRO_RTORRENT_RC": os.devnull},
+                check=True,
+            )
+            shutil.copyfile(torrent_path, watch_dir / torrent_path.name)
 
 
 if __name__ == "__main__":
