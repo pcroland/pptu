pcroland/pptu

commit b8350478e5d6323615672097a2ff3fefa59ebf17

Fix linter errors

pptu/uploaders/hdbits.py
@@ -255,7 +255,7 @@ class HDBitsUploader(Uploader):
         r = self.session.post(
             url="https://img.hdbits.org/upload_api.php",
             files={
-                **{f"images_files[{i}]": open(snap, "rb") for i, snap in enumerate(snapshots)},
+                **{f"images_files[{i}]": snap.read_bytes() for i, snap in enumerate(snapshots)},
                 "thumbsize": f"w{thumbnail_width}",
                 "galleryoption": "1",
                 "galleryname": name,
@@ -292,9 +292,8 @@ class HDBitsUploader(Uploader):
     def upload(self, path, mediainfo, snapshots, *, auto):
         print(self.data, highlight=True)
 
-        if not auto:
-            if not Confirm.ask("\nUpload torrent?"):
-                return False
+        if not auto and not Confirm.ask("\nUpload torrent?"):
+            return False
 
         res = self.session.post(
             url="https://hdbits.org/upload/upload",

pptu/uploaders/_avistaznetwork.py
@@ -243,9 +243,8 @@ class AvistaZNetworkUploader(Uploader, ABC):  # noqa: B024
         }
         print({**data, "_token": "[hidden]", "media_info": "[hidden]"}, highlight=True)
 
-        if not auto:
-            if not Confirm.ask("\nContinue with upload?"):
-                return False
+        if not auto and not Confirm.ask("\nContinue with upload?"):
+            return False
 
         torrent_path = self.dirs.user_cache_path / f"{path.name}_files" / f"{path.name}[{self.abbrev}].torrent"
         url = f"{self.base_url}/upload/{'movie' if collection == 'movie' else 'tv'}"
@@ -327,9 +326,8 @@ class AvistaZNetworkUploader(Uploader, ABC):  # noqa: B024
     def upload(self, path, mediainfo, snapshots, *, auto):
         print(self.data, highlight=True)
 
-        if not auto:
-            if not Confirm.ask("Upload torrent?"):
-                return False
+        if not auto and not Confirm.ask("Upload torrent?"):
+            return False
 
         r = self.session.post(url=self.upload_url, data=self.data, timeout=60)
         res = r.text

pptu/uploaders/broadcasthenet.py
@@ -392,9 +392,8 @@ class BroadcasTheNetUploader(Uploader):
     def upload(self, path, mediainfo, snapshots, *, auto):
         print(self.data, highlight=True)
 
-        if not auto:
-            if not Confirm.ask("\nUpload torrent?"):
-                return False
+        if not auto and not Confirm.ask("\nUpload torrent?"):
+            return False
 
         torrent_path = self.dirs.user_cache_path / f"{path.name}_files" / f"{path.name}[BTN].torrent"
         self.session.post(

pptu/uploaders/passthepopcorn.py
@@ -227,9 +227,8 @@ class PassThePopcornUploader(Uploader):
     def upload(self, path, mediainfo, snapshots, *, auto):
         print(self.data, highlight=True)
 
-        if not auto:
-            if not Confirm.ask("\nUpload torrent?"):
-                return False
+        if not auto and not Confirm.ask("\nUpload torrent?"):
+            return False
 
         res = self.session.post(
             url="https://passthepopcorn.me/upload.php",

commit 7f45c738fa10f3e349301d3acf6ca94bbeee2af0

HDB: Fix snapshot generation broken by b835047

pptu/uploaders/hdbits.py
@@ -1,3 +1,4 @@
+import contextlib
 import hashlib
 import re
 import time
@@ -252,16 +253,17 @@ class HDBitsUploader(Uploader):
         print(f"Using thumbnail width: [bold cyan]{thumbnail_width}[/]")
 
         thumbnails_str = ""
-        r = self.session.post(
-            url="https://img.hdbits.org/upload_api.php",
-            files={
-                **{f"images_files[{i}]": snap.read_bytes() for i, snap in enumerate(snapshots)},
-                "thumbsize": f"w{thumbnail_width}",
-                "galleryoption": "1",
-                "galleryname": name,
-            },
-            timeout=60,
-        )
+        with contextlib.ExitStack() as stack:
+            r = self.session.post(
+                url="https://img.hdbits.org/upload_api.php",
+                files={
+                    **{f"images_files[{i}]": stack.enter_context(snap.open("rb")) for i, snap in enumerate(snapshots)},
+                    "thumbsize": f"w{thumbnail_width}",
+                    "galleryoption": "1",
+                    "galleryname": name,
+                },
+                timeout=60,
+            )
         r.raise_for_status()
         res = r.text
         for i, url in enumerate(res.split()):