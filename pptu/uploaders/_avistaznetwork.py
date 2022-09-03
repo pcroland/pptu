ced2b281f4ee1cb5a258af3608c881e774b7f6fa
nyuszika7h <nyuszika7h@gmail.com>
2022-09-03 15:02:59 +0200

    avistaznetwork: Restore remember=1

--- pptu/uploaders/_avistaznetwork.py
+++ pptu/uploaders/_avistaznetwork.py
@@ -110,6 +110,7 @@ class AvistaZNetworkUploader(Uploader, ABC):  # noqa: B024
                     "email_username": username,
                     "password": password,
                     "captcha": captcha_answer,
+                    "remember": "1",
                 },
             )
             res = r.text
