a53b25228aa619722fa330cbe948f4166f5ddd7a
varyg <88599103+varyg1001@users.noreply.github.com>
2023-08-12 11:41:02 +0200

    bump version

--- pptu/constants.py
+++ pptu/constants.py
@@ -1,4 +1,4 @@
 # TODO: Figure out a way to reliably pull this from pyproject.toml
 #       while taking care of edge cases like pyinstaller
 PROG_NAME = "pptu"
-PROG_VERSION = "2023.08.09"
+PROG_VERSION = "2023.08.12"
