a4b9eb6358aeeba53beb677fbd5c7ae7dbb8eb72
varyg <varygb11@gmail.com>
2024-01-29 12:26:15 +0100

    update PROG_VERSION

--- pptu/constants.py
+++ pptu/constants.py
@@ -1,4 +1,4 @@
 # TODO: Figure out a way to reliably pull this from pyproject.toml
 #       while taking care of edge cases like pyinstaller
 PROG_NAME = "pptu"
-PROG_VERSION = "2023.11.06"
+PROG_VERSION = "2024.01.29"
