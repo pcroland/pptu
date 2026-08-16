69a7ab1d9abd5da6ba290619d0906c5aa9e21234
Varyg <varygb11@gmail.com>
2026-08-16 17:28:11 +0200

    Bump version and update dependencies

--- pptu/__init__.py
+++ pptu/__init__.py
@@ -1,4 +1,4 @@
 # TODO: Figure out a way to reliably pull this from pyproject.toml
 #       while taking care of edge cases like pyinstaller
-__version__ = "2026.07.28"
+__version__ = "2026.07.16"
 PROG_NAME = "pptu"
